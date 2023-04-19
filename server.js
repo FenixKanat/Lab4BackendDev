2a0d8605b7b1c2b0b3ef0ad482e14f4a1773f7f6
roseygirlforra <gulcinkanat97@gmail.com>
2023-04-19 19:59:46 +0200

    Server connects to the database, route for register

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-
-
-const mongoose = require ('mongoose');
-const express = require ('express');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
-
-
-const app = express;
-
+const RegisterNewUser = require('./Register');
 
 mongoose.connect(process.env.MongoDB, {
   useNewUrlParser: true,
@@ -15,4 +11,16 @@ mongoose.connect(process.env.MongoDB, {
   console.log("Connected to MongoDB successfully");
 }).catch((error) => {
   console.log("Error connecting to MongoDB", error);
+});
+
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+  const { userID, userName, userRole, password } = req.body;
+  await RegisterNewUser(userID, userName, userRole, password);
+  res.send('User has been registered successfully.');
+});
+
+app.listen(3333, () => {
+  console.log("Server is running on port 3333");
 });
