fa448ee57a2177ad8a3ff9f3245796e1dadb7568
roseygirlforra <gulcinkanat97@gmail.com>
2023-04-19 20:00:13 +0200

    register logic

--- Register.js
+++ Register.js
@@ -1,3 +1,18 @@
-import users from "./users";
+const users = require('./users');
 
-const express = require ('express');
+async function RegisterNewUser(userID, userName, userRole, password) {
+  const newUser = new users({
+    userID,
+    userName,
+    userRole,
+    password,
+  });
+  try {
+    const registeredUser = await newUser.save();
+    console.log("New user has been saved to the database: ", registeredUser);
+  } catch (err) {
+    console.log("Couldn't register user: ", err);
+  }
+}
+
+module.exports = RegisterNewUser;
