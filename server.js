aec3f2348fe1e5d6087850fe03dec55fdb54b34f
roseygirlforra <gulcinkanat97@gmail.com>
2023-04-21 15:59:03 +0200

    changes to adjust to adminPath.js

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const RegisterNewUser = require('./Register');
 const path = require('path');
-
-
-
-
+const user = require('./users.js');
+const setupAdminPath = require('./adminPath');
 
 mongoose.connect(process.env.MongoDB, {
   useNewUrlParser: true,
@@ -18,23 +16,18 @@ mongoose.connect(process.env.MongoDB, {
   console.log("Error connecting to MongoDB", error);
 });
 
-app.use(express.json());
-//how to render admin.ejs
 app.set('views', path.join(__dirname, 'view'));
 app.set('view engine', 'ejs');
+
+app.use(express.json());
+
+setupAdminPath(app);
 
 app.post('/register', async (req, res) => {
   const { userID, userName, userRole, password } = req.body;
   await RegisterNewUser(userID, userName, userRole, password);
   res.send('User has been registered successfully.');
 });
-
-app.get('/admin', async (req, res) => {
-  const allUsers = await getAllUsers();
-  res.render('admin', { allUsers });
-});
-
-
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
