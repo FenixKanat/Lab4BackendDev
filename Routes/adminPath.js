2366ca79f3d3b05998ca34476a2e57c729fa3707
roseygirlforra <gulcinkanat97@gmail.com>
2023-04-22 14:55:05 +0200

    deleting unused lines

--- Routes/adminPath.js
+++ Routes/adminPath.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../users.js');
-const path = require('path');
 
 router.get('/', async (req, res) => {
   const allUsers = await user.find();
