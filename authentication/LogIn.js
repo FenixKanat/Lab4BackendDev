cf667fe771019c5c12ee405144400700b3e86f96
roseygirlforra <gulcinkanat97@gmail.com>
2023-04-22 14:54:53 +0200

    working on authentication

--- authentication/LogIn.js
+++ authentication/LogIn.js
@@ -1,23 +1,44 @@
 import users from '../users.js';
 import { checkPassword } from './encrypt.js';
+import jwt from 'jsonwebtoken';
+require('dotenv').config();
+
+const secretKey = process.env.secretToken;
 
 async function login(username, password) {
-
   console.log("");
-
   const user = await users.findOne({ username });
   if (!user) {
     return 'Invalid username';
   }
 
-
   if (!checkPassword(password, user.password)) {
     return 'Invalid password';
   }
-  
 
-  return user;
+  // Create the JWT payload
+  const payload = {
+    id: user._id,
+    username: user.username
+  };
 
+  // Sign the JWT with the secret key
+  const token = jwt.sign(payload, secretKey);
+
+  return {
+    user,
+    token
+  };
 }
 
-export default login;
+// Verify a JWT with the secret key
+function verifyToken(token) {
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    return decoded;
+  } catch (err) {
+    return null;
+  }
+}
+
+export { login, verifyToken };
