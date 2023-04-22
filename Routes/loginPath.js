11e952939bbcb6b3ef88006492f707fa03497a6f
roseygirlforra <gulcinkanat97@gmail.com>
2023-04-22 14:53:52 +0200

    render identify, path for start.ejs, edit ejs files

--- Routes/loginPath.js
+++ Routes/loginPath.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../users.js');
-const path = require('path');
+
 
 router.get('/', async (req, res) => {
   const allUsers = await user.find();
-  res.render('login', { data: allUsers });
+  res.render('identify', { data: allUsers });
 });
 
 module.exports = (app) => {
-  app.use('/login', router);
+  app.use('/identify', router);
 };
