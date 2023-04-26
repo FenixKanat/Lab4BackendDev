dc1338b27a53c4d7ee177afc20c93b10f28a286a
roseygirlforra <fenix.k.97@outlook.com>
2023-04-26 22:07:21 +0200

    test user.js

--- users.js
+++ users.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   userID: { type: String, required: true, unique: true },
   userName: { type: String, required: true },
+  udr:{},
   userRole: { type: String, required: true },
   password: { type: String, required: true }
 }, { collection: 'users' });
