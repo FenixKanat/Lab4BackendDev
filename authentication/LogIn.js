474a5f19cd22268da15cdce93a20b284dba4370b
roseygirlforra <gulcinkanat97@gmail.com>
2023-05-01 19:54:10 +0200

    add documentation, and token as variable

--- authentication/LogIn.js
+++ authentication/LogIn.js
@@ -1,28 +1,33 @@
+
 const users = require('../users.js');
 const jwt = require('jsonwebtoken');
 
+var secretKey = process.env.TOKEN_SECRET;
 
+// Login takes two params username and password
 async function login(userName, password) {
   console.log('userName:', userName);
+
+  // Find the user with the given username
   const user = await users.findOne({ userName });
   console.log('user:', user);
 
+  // Throw an error if no user is found with the given username
   if (!user) {
     throw new Error('Invalid username');
   }
 
+  // Throw an error if the given password does not match the users password
   if (user.password !== password) {
     throw new Error('Incorrect password');
   }
 
+  // Generate a JWT token using the user's ID and the secret key
+  const token = jwt.sign({ userID: user.userID }, secretKey);
+  console.log("tokenLogin: " , token);
 
-  // Generate a token
-  const token = jwt.sign({ userID: user.userID }, process.env.TOKEN_SECRET);
-
-  // Return the token and user object if login is successful
+  // Return the token and user if login is successful
   return { token, user };
 }
-
-
 
 module.exports = login;
